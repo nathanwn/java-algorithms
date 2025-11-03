1a59619dc478d8ea14da69a703087951242baa46
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-11-03 21:12:10 +1000

    timus 1001

---
+++ timus/1001/Main.java
@@ -0,0 +1,81 @@
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Solver {
+        void solve(InputReader in, PrintWriter out) {
+            ArrayList<Double> res = new ArrayList<>();
+            while (in.hasNext()) {
+                long roundBy = 10000;
+                long n = in.nextLong();
+                double sqrt = (double) Math.round(Math.sqrt(n) * roundBy) / roundBy;
+                res.add(sqrt);
+            }
+            Collections.reverse(res);
+            for (double x : res) {
+                out.printf("%.4f\n", x);
+            }
+        }
+    }
+
+    static class InputReader {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public InputReader(InputStream inputStream) {
+            reader = new BufferedReader(new InputStreamReader(inputStream), 32768);
+            tokenizer = new StringTokenizer("");
+        }
+
+        public boolean hasNext() {
+            while (!tokenizer.hasMoreTokens()) {
+                String line = null;
+                try {
+                    line = reader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                if (line == null) {
+                    return false;
+                }
+                tokenizer = new StringTokenizer(line);
+            }
+            return true;
+        }
+
+        public String next() {
+            if (!hasNext()) {
+                throw new RuntimeException();
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+        out.close();
+    }
+}
