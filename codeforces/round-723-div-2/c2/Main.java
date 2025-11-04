93f506ed3602cc822e5359c8e0ece36bdd5e3f68
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-11-04 23:57:47 +1000

    codeforces round-723-div-2 c2

---
+++ codeforces/round-723-div-2/c2/Main.java
@@ -0,0 +1,86 @@
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.PriorityQueue;
+
+public class Main {
+    static class Solver {
+        void solve(InputReader in, PrintWriter out) {
+            int n = in.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = in.nextInt();
+            }
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            long cur = 0;
+            for (int i = 0; i < n; i++) {
+                cur += a[i];
+                pq.add(a[i]);
+                while (cur < 0) {
+                    cur -= pq.poll();
+                }
+            }
+            out.println(pq.size());
+        }
+    }
+
+    static class InputReader {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public InputReader(InputStream inputStream) {
+            reader = new BufferedReader(new InputStreamReader(inputStream), 32768);
+            tokenizer = new StringTokenizer("");
+        }
+
+        public boolean hasNext() {
+            while (!tokenizer.hasMoreTokens()) {
+                String line = null;
+                try {
+                    line = reader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                if (line == null) {
+                    return false;
+                }
+                tokenizer = new StringTokenizer(line);
+            }
+            return true;
+        }
+
+        public String next() {
+            if (!hasNext()) {
+                throw new RuntimeException();
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+        out.close();
+    }
+}
