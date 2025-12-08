e2b403e1823f906df611228174a0d6afc3af8aaa
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-12-08 17:42:08 +0700

    aoc template

---
+++ templates/aoc/Main.java
@@ -0,0 +1,20 @@
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        out.println("part 1: " + part1());
+        out.println("part 2: " + part2());
+        out.close();
+    }
+
+    static void part1() {
+    }
+
+    static void part2() {
+    }
+}
