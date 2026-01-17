164ff7de8f1650da27490c4f1db2051ee0feefba
nathanwn <49882031+nathanwn@users.noreply.github.com>
2026-01-18 00:39:19 +1000

    atcoder abc441 a b c d e

---
+++ src/main/java/io/github/nathanwn/structures/FenwickTree.java
@@ -0,0 +1,33 @@
+package io.github.nathanwn.structures;
+
+public class FenwickTree {
+    int n;
+    long[] t;
+
+    public FenwickTree(int n) {
+        this.n = n;
+        t = new long[n];
+    }
+
+    public FenwickTree(int[] a) {
+        n = a.length;
+        t = new long[n];
+        for (int i = 0; i < n; i++) {
+            add(i, a[i]);
+        }
+    }
+
+    public void add(int i, int d) {
+        for (int j = i; j < n; j = j | (j + 1)) {
+            t[j] += d;
+        }
+    }
+
+    public long sum(int i) {
+        long res = 0;
+        for (int j = i; j >= 0; j = (j & (j + 1)) - 1) {
+            res += t[j];
+        }
+        return res;
+    }
+}
