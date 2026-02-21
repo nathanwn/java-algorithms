3bc88d1506de54dff8c2f134ab87612f270e31ea
nathanwn <49882031+nathanwn@users.noreply.github.com>
2026-02-21 10:31:10 +1000

    codeforces round-266-div-2 a

---
+++ archive/codeforces/round-266-div-2/a/Main.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Main {
+    static final boolean MULTITEST = false;
+
+    static void solve() {
+        int numRides = in.nextInt();
+        int numRidesPerMulti = in.nextInt();
+        int priceSingle = in.nextInt();
+        int priceMulti = in.nextInt();
+        if (priceSingle * numRidesPerMulti <= priceMulti) {
+            out.println(numRides * priceSingle);
+        } else {
+            int numMulti = numRides / numRidesPerMulti;
+            int numSingle = numRides % numRidesPerMulti;
+            int ans = numMulti * priceMulti + numSingle * priceSingle;
+            ans = Math.min(ans, (numMulti + 1) * priceMulti);
+            out.println(ans);
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("stress")) {
+            stressTest();
+            return;
+        } else {
+            int t = 1;
+            if (MULTITEST) {
+                t = in.nextInt();
+            }
+            while (t-- > 0) {
+                solve();
+            }
+        }
+        out.close();
+    }
+
+    static void stressTest() {
+        for (int tt = 0; tt < 1000; tt++) {
+            System.err.println("Test #" + tt);
+            long n = random.nextInt(500000) + 1;
+        }
+        System.err.println("AC!");
+    }
+
+    static InputReader in = new InputReader(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+    static Random random = new Random(58);
+
+    static class InputReader {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public InputReader(InputStream inputStream) {
+            reader = new BufferedReader(new InputStreamReader(inputStream), 32768);
+            tokenizer = new StringTokenizer("");
+        }
+
+        public boolean hasNext() {
+            while (!tokenizer.hasMoreTokens()) {
+                String line = null;
+                try {
+                    line = reader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                if (line == null) {
+                    return false;
+                }
+                tokenizer = new StringTokenizer(line);
+            }
+            return true;
+        }
+
+        public String next() {
+            if (!hasNext()) {
+                throw new RuntimeException();
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
